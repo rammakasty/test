eb3707a5a19b456cb516008db34f4213ed9143d9
choi ha young <gkdudk15@naver.com>
2023-02-13 21:10:22 +0900

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // 🔥 반드시 App.css 파일을 import 해줘야 합니다.
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//  User 컴포넌트를 분리해서 구현
+function User(props) {
+    return (
+        <div className="user-card">
+            <div>{props.user.age}살 - </div>
+            <div>{props.user.name}</div>
+        </div>
+    );
 }
+
+const App = () => {
+    const [users, setUsers] = useState([
+        { id: 1, age: 30, name: '송중기' },
+        { id: 2, age: 24, name: '송강' },
+        { id: 3, age: 21, name: '김유정' },
+        { id: 4, age: 29, name: '구교환' },
+    ]);
+    const [name, setName] = useState(''); // <-- 유저의 입력값을 담을 상태
+    const addUserHandler = () => {
+        const newUser = {
+            id: users.length + 1,
+            age: age,
+            name: name,
+        };
+
+        setUsers([...users, newUser]);
+    };
+    return (
+        <div className="app-container">
+            <input
+                value={name}
+                // 인풋 이벤트로 들어온 입력 값을 name의 값으로 업데이트
+                onChange={(e) => setName(e.target.value)}
+            />
+            {users.map((user) => {
+                return <User user={user} key={user.id} />;
+            })}
+            <button onClick={addUserHandler}>추가하기</button>
+        </div>
+    );
+};
 
 export default App;
