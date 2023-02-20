764011236ac23636166e30c7d75f4c76737f5f24
choi ha young <gkdudk15@naver.com>
2023-02-21 00:48:26 +0900

    기본 뷰 틀 수정중

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
 
 const StBox = styled.div`
-    width: 100px;
+    width: 500px;
     height: 100px;
     border: 1px solid ${(props) => props.borderColor};
     margin: 20px;
-    display: inline-flex;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0 1rem;
 `;
 
 function App() {
     return (
         <>
+            <h1>모두의 투두리스트</h1>
+            <hr />
+            <div>무엇을 할까요</div>
             <StBox borderColor="red">박스</StBox>
             <StBox borderColor="blue">박스</StBox>
             <StBox borderColor="green">박스</StBox>
